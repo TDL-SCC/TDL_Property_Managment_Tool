73b9617523237de674269940bbe75ea725ecc961
Joel Balmes <j.balmes@outlook.com>
2020-02-03 20:53:58 -0800

    Added migrations and models for Customer and Reservation tables

---
+++ property-management/app/Customer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    protected $fillable = ['first_name', 'last_name', 'middle_initial', 'phone', 'phone_secondary', 'email', 'date_of_birth'];
+
+    public function reservation() {
+        return $this->hasMany('App/Reservation');
+    }
+}
