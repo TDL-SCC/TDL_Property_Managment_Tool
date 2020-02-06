74b3ee95ccce11d48feceab9cba7a88909762634
TomTrude <60637357+TomTrude@users.noreply.github.com>
2020-02-05 18:11:51 -0800

    Update admin-create-res.blade.php

--- property-management/resources/views/admin-create-res.blade.php
+++ property-management/resources/views/admin-create-res.blade.php
@@ -5,9 +5,14 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Create Reservation') }}</div>
+                <div class="card-header">{{ __('Create New Reservation') }}</div>
+
+                
 
                 <div class="card-body">
+                <div class="form-group row">
+                    <strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Step 1: Register New Guest</strong>
+                </div>
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
@@ -40,10 +45,10 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="middle_initial" class="col-md-4 col-form-label text-md-right">{{ __('Middle Initial(Change Type stored to char)') }}</label>
+                            <label for="middle_initial" class="col-md-4 col-form-label text-md-right">{{ __('Middle Initial') }}</label>
                             
-                            <div class="col-md-6"> 
-                                <input id="middle_initial" type="text" class="form-control @error('middle_initial') is-invalid @enderror" name="middle_initial" value="{{ old('middle_initial') }}" required autocomplete="name">
+                            <div class="col-md-2"> 
+                                <input id="middle_initial" type="char" class="form-control @error('middle_initial') is-invalid @enderror" name="middle_initial" value="{{ old('middle_initial') }}" required autocomplete="name">
 
                                 @error('middle_initial')
                                     <span class="invalid-feedback" role="alert">
@@ -56,7 +61,7 @@
                         <div class="form-group row">
                             <label for="phone" class="col-md-4 col-form-label text-md-right">{{ __('Phone Number Primary') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-3">
                                 <input id="phone" type="text" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ old('phone') }}" required autocomplete="name">
 
                                 @error('phone')
@@ -70,7 +75,7 @@
                         <div class="form-group row">
                             <label for="phone_secondary" class="col-md-4 col-form-label text-md-right">{{ __('Phone Number Secondary') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-3">
                                 <input id="phone_secondary" type="text" class="form-control @error('phone_secondary') is-invalid @enderror" name="phone_secondary" value="{{ old('phone_secondary') }}" required autocomplete="name">
 
                                 @error('phone_secondary')
@@ -98,8 +103,8 @@
                         <div class="form-group row">
                             <label for="date_of_birth" class="col-md-4 col-form-label text-md-right">{{ __('Date of Birth') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="date_of_birth" type="email" class="form-control @error('date_of_birth') is-invalid @enderror" name="date_of_birth" value="{{ old('date_of_birth') }}" required autocomplete="date_of_birth">
+                            <div class="col-md-4">
+                                <input id="date_of_birth" type="date" class="form-control @error('date_of_birth') is-invalid @enderror" name="date_of_birth" value="{{ old('date_of_birth') }}" required autocomplete="date_of_birth">
 
                                 @error('date_of_birth')
                                     <span class="invalid-feedback" role="alert">
@@ -112,7 +117,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
+                                    {{ __('Register New Guest') }}
                                 </button>
                             </div>
                         </div>
