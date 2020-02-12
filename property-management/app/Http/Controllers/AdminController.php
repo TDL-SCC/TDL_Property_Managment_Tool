ed8c0bc09093159b0bea366f8c14744e71433d7f
Gabe <GerbertDev@users.noreply.github.com>
2020-02-11 23:09:34 -0800

    Added View and Update functionality for reservations and customers.

--- property-management/app/Http/Controllers/AdminController.php
+++ property-management/app/Http/Controllers/AdminController.php
@@ -33,6 +33,14 @@ class AdminController extends Controller
         return view('admin-create-res');
     }
 
+    public function showAdminShowUpdateResForm($id)
+    {
+        $reservation = Reservation::find($id);
+        $customer = Customer::find($reservation['customer_id']);
+
+        return view('admin-show-update-res', compact('id', 'reservation', 'customer'));
+    }
+
     public function postAdminCreateRes(Request $request)
     {
         $this->validate($request, [
@@ -75,6 +83,49 @@ class AdminController extends Controller
 
 
         return redirect()->route('admin.dashboard');
+    }
+
+    public function postAdminUpdateRes(Request $request)
+    {
+        $this->validate($request, [
+            'first_name' => 'required|min:1',
+            'last_name' => 'required|min:3',
+            'middle_initial' => 'required|min:1',
+            'phone' => 'required|min:10',
+            'phone_secondary' => 'required|min:10',
+            'email' => 'required|min:5',
+            'date_of_birth' => 'required',
+            'check_in_date' => 'required',
+            'check_out_date' => 'required',
+            'room_type' => 'required|min:3',
+            'room_number' => 'required|min:4'
+        ]);
+
+        $id = $request->input('reservationId');
+
+        $reservation = Reservation::find($id);
+
+        $reservation['check_in_date'] = $request->input('check_in_date');
+        $reservation['check_out_date'] = $request->input('check_out_date');
+        $reservation['room_type'] = $request->input('room_type');
+        $reservation['room_number'] = $request->input('room_number');
+
+        $reservation->save();
+
+        $customer = Customer::find($reservation['customer_id']);
+
+        $customer['first_name'] = $request->input('first_name');
+        $customer['last_name'] = $request->input('last_name');
+        $customer['middle_initial'] = $request->input('middle_initial');
+        $customer['phone'] = $request->input('phone');
+        $customer['phone_secondary'] = $request->input('phone_secondary');
+        $customer['email'] = $request->input('email');
+        $customer['date_of_birth'] = $request->input('date_of_birth');
+            
+        $customer->save();
+
+
+        return redirect()->route('admin.get-all-reservations');
     }
 
     public function getAllReservations(){
--- property-management/routes/web.php
+++ property-management/routes/web.php
@@ -22,8 +22,14 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::prefix('admin')->group(function() {
     Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
     Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+
     Route::get('/create-reservation', 'AdminController@showAdminCreateResForm')->name('admin.create-res');
     Route::post('/create-reservation', 'AdminController@postAdminCreateRes')->name('admin.create-res.submit');
+
+    Route::get('/update-reservation/{id}', 'AdminController@showAdminShowUpdateResForm')->name('admin.show-update-res');
+    Route::post('/update-reservation', 'AdminController@postAdminUpdateRes')->name('admin.update-res.submit');
+
     Route::get('/', 'AdminController@index')->name('admin.dashboard');
+
     Route::get('/get-all-reservations', 'AdminController@getAllReservations')->name('admin.get-all-reservations');
 });
